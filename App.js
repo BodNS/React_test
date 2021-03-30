bf7f7469d5193761eac00d432f7fb2557731e07d
BodNS <71136538+BodNS@users.noreply.github.com>
2021-03-30 23:26:13 +0300

    Add files via upload
    5

--- App.js
+++ App.js
@@ -1,28 +1,31 @@
 import "./App.css";
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
   // <UserList  users={users} />
-  
+
+  const [users, setUsers] = useState([]);
 
   const UsersToLi = (users) => {
-    
-   /*  users.map((e) => {
+    /*  users.map((e) => {
       <li key={e.id}>{`${e.id} ${e.fName} ${e.lName}`}</li>
     }); */
+    console.log(users);
 
-  
-      [...users].forEach((e) => {
-        console.log(e)
-      })
-
+    // [...users].forEach((e) => {
+    //   console.log(e);
+    // });
   };
 
-  fetch('../public/data/users.json')
-      .then((response) => response.json())
-      .then(UsersToLi)
-      .catch(console.error);
+  fetch("../public/data/users.json")
+    .then((response) => response.json())
+    .then((items) => setUsers((users = items)))
+    .catch(console.error);
 
-  return <ul><UsersToLi /></ul>;
+  return (
+    <ul>
+      <UsersToLi />
+    </ul>
+  );
 }
 export default App;
