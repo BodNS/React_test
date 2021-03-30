582f438f8fe4725c2aaec908d6d24411415be355
BodNS <71136538+BodNS@users.noreply.github.com>
2021-03-30 17:59:04 +0300

    Add files via upload
    4

--- App.js
+++ App.js
@@ -3,16 +3,25 @@ import React from "react";
 
 function App() {
   // <UserList  users={users} />
-  fetch("../public/data/users.json")
+  
+
+  const UsersToLi = (users) => {
+    
+   /*  users.map((e) => {
+      <li key={e.id}>{`${e.id} ${e.fName} ${e.lName}`}</li>
+    }); */
+
+  
+      [...users].forEach((e) => {
+        console.log(e)
+      })
+
+  };
+
+  fetch('../public/data/users.json')
       .then((response) => response.json())
       .then(UsersToLi)
       .catch(console.error);
-
-  const UsersToLi = (users) => {
-    users.map((e) => {
-      <li key={e.id}>{`${e.id} ${e.fName} ${e.lName}`}</li>
-    });
-  };
 
   return <ul><UsersToLi /></ul>;
 }
